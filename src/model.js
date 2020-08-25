39b588c88725daa38159a6f12cbb37e1c04d01cd
Vasili Podoplielov <vasili.podoplelov@gmail.com>
2020-08-25 22:09:20 +0300

    add start html structure. and start model

---
+++ src/model.js
@@ -0,0 +1,25 @@
+export const model = [
+  {
+    type: 'title',
+    value: 'Test title',
+    options: {
+
+    }
+  },
+  {
+    type: 'text',
+    value: 'lorem ipsum dolor sit amet',
+    options: {
+
+    }
+  },
+  {
+    type: 'textColumns',
+    value: [
+      '1 text', '2 text', '3 text', '4 text'
+    ],
+    options: {
+
+    }
+  }
+];
